ee38167d865269833063bfaef114136d23b395c7
Alexander <alex@brilliantov.me>
2014-02-26 14:57:33 +0300

    Fixing fatal error in PHP 5.3.28
    Fatal error: Cannot re-assign auto-global variable _COOKIE

--- class.gaparse.php
+++ class.gaparse.php
@@ -32,7 +32,7 @@ class GA_Parse
   var $pages_viewed;			// Pages viewed in current session
   
   
-  function __construct($_COOKIE) {
+  function __construct($cookie = false) {
 	   // If we have the cookies we can go ahead and parse them.
 	   if (isset($_COOKIE["__utma"]) and isset($_COOKIE["__utmz"])) {
 	       $this->ParseCookies();      
